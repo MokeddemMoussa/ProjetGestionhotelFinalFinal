6eecaaacde48bf98901bfb53f94b75f10abe03da
Mokeddem moussa <mokeddem.moussa@yahoo.com>
2023-04-15 01:57:43 -0800

    fixer le problem d'excution

---
+++ Hotel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetGestionhotelFinal
+{
+    internal class Hotel
+    {
+        
+            public ArrayList Clients { get; set; }
+
+            public Hotel()
+            {
+                Clients = new ArrayList();
+            }
+
+            public void AjouterClient(Client client)
+            {
+                Clients.Add(client);
+            }
+        
+
+    }
+}
