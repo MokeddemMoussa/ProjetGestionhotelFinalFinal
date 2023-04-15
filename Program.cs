53af876aa786c9eacac01765516771d3d272f4a8
Mokeddem moussa <mokeddem.moussa@yahoo.com>
2023-04-15 02:10:01 -0800

    Finaliser le code et fixer le problem d'excution

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using ProjetGestionhotelFinal;
-
 class Program
 {
 
@@ -9,37 +8,45 @@ class Program
 
         while (true)
         {
-            // Demander les informations du client 
-            Console.WriteLine("Bonjour monsieur ,S'il vous plait entrez votre nom");
+            // Demander les informations du nouveau client de notre hotel 
+            Console.WriteLine("Hello sir, Welcome to Hilton Hotel please enter your name.");
             string nom = Console.ReadLine();
             if (nom.ToLower() == "q")
                 break;
 
-            Console.WriteLine("Entrez votre prenom s'il vous plait  :");
+            Console.WriteLine("Please enter your first name:");
             string prenom = Console.ReadLine();
 
-            Console.WriteLine("Entrez votre courriel s'il vous plait  ::");
+            Console.WriteLine("Please enter your email address");
             string courriel = Console.ReadLine();
 
-            Console.WriteLine("Entrez votre numéro du carte de crédit s'il vous plait  ::");
+            Console.WriteLine("Please enter your credit card number:\"");
             string carteCredit = Console.ReadLine();
 
-            Console.WriteLine("Entrez votre numéro de la chambre s'il vous plait  : :");
+            Console.WriteLine("Please enter your room number");
             int numeroChambre = int.Parse(Console.ReadLine());
 
             // Créer un nouveau client et l'ajouter à la liste de clients de l'hôtel
             Client nouveauClient = new Client(nom, prenom, courriel, carteCredit, numeroChambre);
             hotel.AjouterClient(nouveauClient);
 
-            Console.WriteLine("Merci pour votre Reservation.");
+            // Afficher les informations du nouveau client ajouté
+            Console.WriteLine("Nom : {0}, Prénom : {1}, Courriel : {2}, Carte de crédit : {3}, Numéro de chambre : {4}",
+                nouveauClient.Nom, nouveauClient.Prenom, nouveauClient.Courriel, nouveauClient.CarteCredit, nouveauClient.NumeroChambre);
+
+            Console.WriteLine("Thank you for your reservation.");
         }
 
-        Console.WriteLine("voila vos information :");
+        Console.WriteLine("these are your  informations :");
+
+        // Boucle foreach pour afficher les informations de chaque client
         foreach (Client client in hotel.Clients)
         {
-            Console.WriteLine("{0} {1} - Chambre {2}", client.Prenom, client.Nom, client.NumeroChambre);
+            Console.WriteLine("Nom: {0}Prenom: {1}Courriel: {2}Carte de credit: {3}Numero de chambre: {4}",
+                              client.Nom , client.Prenom,  client.Courriel,  client.CarteCredit,  client.NumeroChambre);
         }
 
         Console.ReadLine();
     }
+
 }
